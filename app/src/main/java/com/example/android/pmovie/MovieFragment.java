20f138b49b8e5d01e1aae7c23df07c76adf22058
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-07-23 20:57:04 -0500

    get movie json string

--- app/src/main/java/com/example/android/pmovie/MovieFragment.java
+++ app/src/main/java/com/example/android/pmovie/MovieFragment.java
@@ -1,9 +1,12 @@
 package com.example.android.pmovie;
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.internal.widget.AdapterViewCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,16 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 /**
  * Created by zz on 7/22/2015.
@@ -31,6 +44,9 @@ public class MovieFragment extends Fragment {
     public void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         mContext = this.getActivity().getApplicationContext();
+
+        FetchMovieTask movieTask = new FetchMovieTask();
+        movieTask.execute();
     }
 
     @Override
@@ -96,4 +112,71 @@ public class MovieFragment extends Fragment {
         };
     }
 
+    public class FetchMovieTask extends AsyncTask<String, Void, List> {
+
+        @Override
+        protected List doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String movieJsonStr = null;
+            try {
+                final String BASE_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
+                Uri buildUri = Uri.parse(BASE_URL).buildUpon().build();
+                URL url = new URL(buildUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a lot easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream is empty. No point in parsing.
+                    return null;
+                }
+
+                movieJsonStr = buffer.toString();
+                Log.d(LOG_TAG, "Movie Json = " + movieJsonStr);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+
+            return null;
+        }
+
+        protected void onPostExecute(List result) {
+
+        }
+    }
 }
